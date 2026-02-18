5b758c06ddab1b15752051f3e8de48b5d4257f9c
ollyyop <oparsheva12@gmail.com>
2026-02-18 10:27:51 +0300

    Initial Commit

---
+++ WebCalculator/WebCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCalculatorApp.swift
+//  WebCalculator
+//
+//  Created by Olya Parsheva on 18.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WebCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
